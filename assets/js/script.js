866f9ddd134c8fd233ff905a1c0a3c20bb11ee9a
mahisamaliha <maishamaliha71@gmail.com>
2023-05-08 20:17:58 +0600

    responsive changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,12 @@ $('.service-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
+        768:{
+            items:2
+        },
+        1200:{
             items:3
         },
-        1000:{
-            items:3
-        }
     }
 })
 
@@ -24,10 +24,7 @@ $('.project-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
-            items:2
-        },
-        1000:{
+        768:{
             items:2
         }
     }
@@ -35,42 +32,39 @@ $('.project-carousel').owlCarousel({
 
 $('.testomonial-carousel').owlCarousel({
     loop:true,
-    margin:180,
+    margin:20,
     nav:true,
     dots: false,
     responsive:{
         0:{
             items:1
         },
-        600:{
-            items:2
-        },
-        1000:{
+        992:{
             items:2
         }
     }
 })
 
-    // Navbar offcanvas dropdown toggle starts
-    const navbarOffCanvas = document.querySelector(".offcanvas.navbar-content__item");
-    if (navbarOffCanvas) {
-        navbarOffCanvas.addEventListener('show.bs.offcanvas', event => {
-            $(".offcanvas.navbar-content__item .offcanvas-body .nav-item a").on("click", function () {
-                var $this = $(this);
-                if ($this.hasClass("dropdown-toggle")) {
-                    $this.toggleClass("icon-rotate");
-                    $this.next().slideToggle();
-                    if ($this.parent().siblings().children().hasClass("dropdown-toggle")) {
-                        $this.parent().siblings().children().removeClass("icon-rotate");
-                        $this.parent().siblings().children().next().slideUp();
-                    }
+// Navbar offcanvas dropdown toggle starts
+const navbarOffCanvas = document.querySelector(".offcanvas.navbar-content__item");
+if (navbarOffCanvas) {
+    navbarOffCanvas.addEventListener('show.bs.offcanvas', event => {
+        $(".offcanvas.navbar-content__item .offcanvas-body .nav-item a").on("click", function () {
+            var $this = $(this);
+            if ($this.hasClass("dropdown-toggle")) {
+                $this.toggleClass("icon-rotate");
+                $this.next().slideToggle();
+                if ($this.parent().siblings().children().hasClass("dropdown-toggle")) {
+                    $this.parent().siblings().children().removeClass("icon-rotate");
+                    $this.parent().siblings().children().next().slideUp();
                 }
-            });
-        })
+            }
+        });
+    })
 
-        // When navbar offcanvas gets hide
-        navbarOffCanvas.addEventListener('hide.bs.offcanvas', event => {
-            $(".nav-item a").removeClass("icon-rotate");
-            $(".nav-item a").next().slideUp();
-        })
-    }
+    // When navbar offcanvas gets hide
+    navbarOffCanvas.addEventListener('hide.bs.offcanvas', event => {
+        $(".nav-item a").removeClass("icon-rotate");
+        $(".nav-item a").next().slideUp();
+    })
+}
